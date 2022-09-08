570b47c2a3227e42586f2c4d21abb31e2a08c658
mkaychuks <sachufusi@gmail.com>
2022-09-08 18:45:14 +0100

    configure the navigation and provider

--- App.js
+++ App.js
@@ -2,17 +2,33 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
 
 import Home from "./src/screens/Home";
 import { store } from "./src/store";
+import Login from "./src/screens/Login";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-        <Home />
-      </View>
+      <StatusBar style="auto" />
+
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerTitleAlign: "center" }}
+          />
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerTitleAlign: "center" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
@@ -21,7 +37,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
   },
 });
